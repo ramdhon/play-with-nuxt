41082e817172c2567726067ee8d76cfcca4f7496
Muhammad Ramadhoni <ramadhoni@keyreply.com>
2020-10-22 01:00:00 +0700

    adding config to auto fix eslint

--- nuxt.config.js
+++ nuxt.config.js
@@ -66,5 +66,20 @@ export default {
   },
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {},
+  build: {
+    extend(config, ctx) {
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: "pre",
+          test: /\.(js|vue)$/,
+          loader: "eslint-loader",
+          exclude: /(node_modules)/,
+          options: {
+            fix: true
+          }
+        })
+      }
+    }
+  },
 }
